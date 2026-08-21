fc477fb18df1fa920616c1157f0fee0d0426e7bb
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-21 15:55:33 -0300

    refacor: remove middlewares, rotas e configs de autenticação  Mantem apenas operações de crud

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ import 'dotenv/config';
 import { App } from './src/App.js';
 import { Router } from './src/core/Router.js';
 import { HomeController } from './src/controllers/HomeController.js';
-import { AuthController } from './src/controllers/AuthController.js';
 import { ProductController } from './src/controllers/ProductController.js';
 
 const app = new App();
 const router = new Router();
 const home = new HomeController();
-const auth = new AuthController();
 const products = new ProductController();
 
 router.get('/', home, 'index');
-router.get('/login', auth, 'login');
-router.post('/login', auth, 'authenticate');
-router.post('/logout', auth, 'logout');
 router.get('/produtos', products, 'index');
+router.get('/produtos/novo', products, 'create');
 router.post('/produtos', products, 'store');
+router.get('/produtos/:id', products, 'show');
+router.get('/produtos/:id/editar', products, 'edit');
+router.post('/produtos/:id', products, 'update');
 router.post('/produtos/:id/delete', products, 'destroy');
 router.get('/api/produtos', products, 'apiIndex');
 router.get('/api/products', products, 'apiIndex');
