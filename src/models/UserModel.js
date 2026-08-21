4b0797bf76875af8f64bee08f39121120e930c65
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-21 15:52:21 -0300

    refactor: remove scripts para login

--- src/models/UserModel.js
+++
@@ -1,8 +0,0 @@
-import { BaseModel } from './BaseModel.js';
-
-export class UserModel extends BaseModel {
-  async findByEmail(email) {
-    const result = await this.query('SELECT id, name, email, password FROM users WHERE email = $1 LIMIT 1', [email]);
-    return result.rows[0] || null;
-  }
-}
